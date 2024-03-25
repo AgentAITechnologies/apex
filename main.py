572b2ce433222b948880cc3d5b92ec058c8715f7
MysteryMooCows <rod.j.myers@gmail.com>
2024-03-24 21:21:04 -0400

    created state_management.py

--- main.py
+++ main.py
@@ -3,46 +3,53 @@ import os
 import dotenv
 import json
 
+from state_management import ConversationState, ConversationStateMachine
+
 
 
 dotenv.load_dotenv()
 
 
-# TODO: External JSON
-
 CLI_STRS = {
     "init": "What would you like me to do? > "
 }
 
-SYS_PROMTPS = {
-    "need_state": "You are a helpful agent that controls a desktop PC who takes actions on behalf of the computer's user. You have access to a Python interpreter, a shell for the underlying system, the screen (by taking and viewing screenshots), and a keyboard and mouse emulator (via the Python interpreter). You may determine the current state of the system programmatically and/or by viewing a screenshot. A user is now asking you to take action on their behalf. It is now your job to determine if you need additional information to complete the task. If you need to ask the user a question to better understand your task, respond with a JSON object containing the property \"question\" that contains a sub-property \"reason\" that contains the reason why asking the user a question would be most helpful. If you need more context about the system's current state and it can best be attained programmatically through the Python interpreter, respond with a JSON object containing the property \"python\" that contains a sub-property \"reason\" that contains the reason why executing Python code to get the required state information would be most helpful, and another sub-property \"privileged\" set to \"true\" for scripts that require privilege execution and set to \"false\" otherwise. If a PowerShell command would be most effective to get the required state, respond with a JSON object containing the property \"powershell\" that contains a sub-property \"reason\" that contains the reason why executing a PowerShell script to get the required state information would be most helpful, and another sub-property \"privileged\" set to \"true\" for scripts that require privilege execution and set to \"false\" otherwise. If a current screenshot would be most effective to get the required state, respond with a JSON object containing the property \"screenshot\" that contains a sub-property \"reason\" that contains the reason getting a screenshot would be most helpful. If you believe action may be taken to accomplish the user's goal without needing to know anything additional from the user or the system, where that action may be taken by either executing a Python script, PowerShell script, or mouse and keyboard emulation, respond with a JSON object containing the property \"ready\" that contains a sub-property \"reason\" that contains the reason you feel ready to perform the action.",
-}
-
-
-
 def main():
-    task = input(CLI_STRS["init"])
-
     client = anthropic.Anthropic(
         api_key=os.environ.get("ANTHROPIC_API_KEY"),
     )
+    
+    with open(os.path.join(os.environ.get("INPUT_DIR"), "states.json")) as file:
+        state_data = json.load(file)
+
+    with open(os.path.join(os.environ.get("INPUT_DIR"), "transitions.json")) as file:
+        transition_data = json.load(file)
+
+    csm = ConversationStateMachine(state_data=state_data, transition_data=transition_data, init_state_path='start')
+    csm.transition("selectready")
+
+    task = input(CLI_STRS["init"])
+
+    test_frmt = {
+        "task": task
+    }
+
+    test_messages = [{
+            "role":  csm.current_state.messages[0]["role"],
+            "content": [{
+                    "type": csm.current_state.messages[0]["content"][0]["type"],
+                    "text": csm.current_state.messages[0]["content"][0]["text"].format(**test_frmt)
+                }]
+        }]
+    
+    test_system = csm.current_state.system.format(**test_frmt)
 
     message = client.messages.create(
         model=os.environ.get("MODEL"),
         max_tokens=4000,
         temperature=0,
-        system=SYS_PROMTPS["need_state"],
-        messages=[
-            {
-                "role": "user",
-                "content": [
-                    {
-                        "type": "text",
-                        "text": task
-                    }
-                ]
-            }
-        ]
+        system=test_system,
+        messages=test_messages
     )
 
     if message.content[0].type == "text":
---
+++ state_management.py
@@ -0,0 +1,170 @@
+import json
+import pygraphviz as pgv
+import os
+
+
+class ConversationState:
+    def __init__(self, name=None, parent=None, system="", messages=[], frmtstrs={}):
+        self.name = name
+
+        self.system = system
+        self.messages = messages
+        self.frmtstrs = frmtstrs
+
+        self.parent = parent
+
+        self.transitions = {}
+        self.children = []
+
+    def add_message(self, message):
+        self.messages.append(message)
+
+    def add_transition(self, trigger, next_state):
+        self.transitions[trigger] = next_state
+
+    def add_child(self, child_state):
+        self.children.append(child_state)
+        child_state.parent = self
+
+    def get_next_state(self, response):
+        if response in self.transitions:
+            return self.transitions[response]
+        elif self.parent:
+            return self.parent.get_next_state(response)
+        else:
+            return None
+
+    def get_root(self):
+        if self.parent:
+            return self.parent.get_root()
+        else:
+            return self
+
+    def get_hierarchy_path(self):
+        if self.parent and self.parent.name != "root":
+            return self.parent.get_hierarchy_path() + "_" + self.name
+        else:
+            return self.name
+
+
+class ConversationStateMachine:
+    PRINT_PREFIX = "[CSM]"
+
+    def __init__(self, state_data=None, transition_data=None, init_state_path=None):
+        self.initialize_conversation_states(state_data)
+        self.initialize_transitions(transition_data)
+        self.current_state = self.state_map[init_state_path]
+
+    def transition(self, trigger):
+        if trigger in self.current_state.transitions:
+            self.current_state = self.current_state.transitions[trigger]
+            return self.current_state
+        else:
+            print(f"{self.PRINT_PREFIX} invalid trigger '{trigger}' for state {self.current_state.get_hierarchy_path()}")
+            return None
+
+    def initialize_conversation_states(self, state_data):
+        def create_state(state_data, parent=None):
+            state = ConversationState(name=state_data["name"],
+                                      parent=parent,
+                                      system=state_data.get("system", ""),
+                                      messages=state_data.get("messages", []),
+                                      frmtstrs = state_data.get("frmtstrs", {}))
+
+            for child_data in state_data.get("children", []):
+                child_state = create_state(child_data, parent=state)
+                state.add_child(child_state)
+
+            return state
+
+        self.root_state = create_state(state_data)
+
+    def find_state_by_path(self, path):
+            return self.state_map.get(path)
+    
+    def initialize_transitions(self, transition_data=None):
+        self.transition_data = transition_data
+        self.state_map = {}
+
+        def traverse_and_map_states(state):
+            self.state_map[state.get_hierarchy_path()] = state
+            for child in state.children:
+                traverse_and_map_states(child)
+
+        traverse_and_map_states(self.root_state)
+
+        for transition in transition_data:
+            trigger = transition["trigger"]
+            source_paths = transition["source"]
+            dest_path = transition["dest"]
+
+            if not isinstance(source_paths, list):
+                source_paths = [source_paths]
+
+            for source_path in source_paths:
+                source_path = source_path
+                source_state = self.find_state_by_path(source_path)
+                dest_state = self.find_state_by_path(dest_path)
+
+                if source_state and dest_state:
+                    source_state.add_transition(trigger, dest_state)
+                else:
+                    print(f"{self.PRINT_PREFIX} Warning: Invalid transition - Source: {source_path}, Destination: {dest_path}")
+
+    def visualize(self):
+        graph = pgv.AGraph(directed=True)
+
+        graph.graph_attr['fontname'] = 'Consolas'
+        graph.node_attr['fontname'] = 'Consolas'
+        graph.node_attr['shape'] = 'box'
+        graph.node_attr['style'] = 'rounded'
+        graph.edge_attr['fontname'] = 'Consolas'
+
+        def add_state_to_graph(state, parent_subgraph=None):
+            if parent_subgraph is None:
+                subgraph = graph
+            else:
+                subgraph = parent_subgraph.add_subgraph(name=f"cluster_{state.get_hierarchy_path()}")
+                subgraph.graph_attr['style'] = 'rounded'
+
+            if not (parent_subgraph is None):
+                subgraph.add_node(state.get_hierarchy_path(), label=state.name)
+
+            for child in state.children:
+                add_state_to_graph(child, subgraph)
+
+        add_state_to_graph(self.root_state)
+
+        for transition in self.transition_data:
+            trigger = transition["trigger"]
+            source_paths = transition["source"]
+            dest_path = transition["dest"]
+
+            if not isinstance(source_paths, list):
+                source_paths = [source_paths]
+
+            for source_path in source_paths:
+                source_state = graph.get_node(source_path)
+                dest_state = graph.get_node(dest_path)
+
+                if source_state and dest_state:
+                    graph.add_edge(source_state, dest_state, label=trigger)
+
+        graph.layout(prog='dot')
+        
+        if not os.path.exists(os.environ.get("OUTPUT_DIR")):
+            os.makedirs(os.environ.get("OUTPUT_DIR"))
+            
+        graph.draw(os.path.join(os.environ.get("OUTPUT_DIR"), 'state_diagram.png'))
+     
+    def print_current_state(self):
+        print(f"{self.PRINT_PREFIX} self.current_state: {self.current_state}")
+        print(f"{self.PRINT_PREFIX} self.current_state.get_hierarchy_path(): {self.current_state.get_hierarchy_path()}")
+
+    def print_state_hierarchy(self, state=None, level=0):
+        if state == None:
+            state = self.root_state
+
+        print(self.PRINT_PREFIX + "  " * (level+1) + state.get_hierarchy_path())
+        for child in state.children:
+            self.print_state_hierarchy(child, level + 1)
